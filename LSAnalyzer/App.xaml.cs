348e088ca11fe844ac12bceeced1bb39107c163d
k.oberwimmer <k.oberwimmer@NBK025>
2023-07-23 10:00:59 +0200

    first usage of R engine

--- LSAnalyzer/App.xaml.cs
+++ LSAnalyzer/App.xaml.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using RDotNet;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace LSAnalyzer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            using var engine = REngine.GetInstance();
+            string[] a = engine.Evaluate("'Hi there .NET, from the R engine'").AsCharacter().ToArray();
+
+            MainWindow window = _serviceProvider.GetRequiredService<MainWindow>();
+            window.Title = a[0];
+            window.Show();
+        }
     }
 }
