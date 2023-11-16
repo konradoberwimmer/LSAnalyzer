4f180af741ec01f4452f2220f3a40758921e83ad
Konrad Oberwimmer <k.oberwimmer@bifie.at>
2023-11-16 11:33:45 +0100

    provided service for batch analyzing

---
+++ LSAnalyzer/Services/BatchAnalyze.cs
@@ -0,0 +1,168 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using LSAnalyzer.Models;
+using LSAnalyzer.ViewModels;
+using RDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSAnalyzer.Services
+{
+    public class BatchAnalyze
+    {
+        private Rservice _rservice;
+        private bool _useCurrentFile = true;
+        private bool _currentModeKeep = true;
+
+        public BatchAnalyze(Rservice rservice)
+        {
+            _rservice = rservice;
+        }
+
+        public void RunBatch(Dictionary<int, Analysis> analyses, bool useCurrentFile, bool currentModeKeep = true)
+        {
+            _useCurrentFile = useCurrentFile;
+            _currentModeKeep = currentModeKeep;
+
+            BackgroundWorker worker = new();
+            worker.WorkerReportsProgress = false;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += DoBatch;
+            worker.RunWorkerAsync(analyses);
+        }
+
+        private void DoBatch(object? sender, DoWorkEventArgs e)
+        {
+            if (e.Argument is not Dictionary<int, Analysis> analyses)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            foreach (var analysis in analyses)
+            {
+                if (!_useCurrentFile)
+                {
+                    if (!_rservice.TestAnalysisConfiguration(analysis.Value.AnalysisConfiguration, analysis.Value.SubsettingExpression))
+                    {
+                        WeakReferenceMessenger.Default.Send(new BatchAnalyzeMessage()
+                        {
+                            Id = analysis.Key,
+                            Success = false,
+                            Message = "Could not load file '" + analysis.Value.AnalysisConfiguration.FileName + "' or use it for dataset type '" + analysis.Value.AnalysisConfiguration.DatasetType?.Name + "'!"
+                        });
+                        continue;
+                    }
+
+                    if (analysis.Value.AnalysisConfiguration.ModeKeep == false)
+                    {
+                        List<string>? additionalVariables = null;
+                        if (analysis.Value is AnalysisRegression analysisRegression)
+                        {
+                            additionalVariables = new() { analysisRegression.Dependent?.Name ?? "insufficient_analysis_definition_" };
+                        }
+                        if (!_rservice.PrepareForAnalysis(analysis.Value, additionalVariables))
+                        {
+                            WeakReferenceMessenger.Default.Send(new BatchAnalyzeMessage()
+                            {
+                                Id = analysis.Key,
+                                Success = false,
+                                Message = "Could not build file '" + analysis.Value.AnalysisConfiguration.FileName + "' for analysis!"
+                            });
+                            continue;
+                        }
+                    }
+                }
+
+                if (_useCurrentFile && !_currentModeKeep)
+                {
+                    List<string>? additionalVariables = null;
+                    if (analysis.Value is AnalysisRegression analysisRegression)
+                    {
+                        additionalVariables = new() { analysisRegression.Dependent?.Name ?? "insufficient_analysis_definition_" };
+                    }
+                    if (!_rservice.PrepareForAnalysis(analysis.Value, additionalVariables))
+                    {
+                        WeakReferenceMessenger.Default.Send(new BatchAnalyzeMessage()
+                        {
+                            Id = analysis.Key,
+                            Success = false,
+                            Message = "Could not build current file for analysis!"
+                        });
+                        continue;
+                    }
+                }
+
+                DateTime beforeCalculation = DateTime.Now;
+
+                switch (analysis.Value)
+                {
+                    case AnalysisUnivar analysisUnivar:
+                        analysisUnivar.Result = _rservice.CalculateUnivar(analysisUnivar) ?? new();
+                        break;
+                    case AnalysisMeanDiff analysisMeanDiff:
+                        analysisMeanDiff.Result = _rservice.CalculateMeanDiff(analysisMeanDiff) ?? new();
+                        break;
+                    case AnalysisFreq analysisFreq:
+                        analysisFreq.Result = _rservice.CalculateFreq(analysisFreq) ?? new();
+                        if (analysisFreq.CalculateBivariate)
+                        {
+                            analysisFreq.BivariateResult = _rservice.CalculateBivariate(analysisFreq);
+                        }
+                        break;
+                    case AnalysisPercentiles analysisPercentiles:
+                        analysisPercentiles.Result = _rservice.CalculatePercentiles(analysisPercentiles) ?? new();
+                        break;
+                    case AnalysisCorr analysisCorr:
+                        analysisCorr.Result = _rservice.CalculateCorr(analysisCorr) ?? new();
+                        break;
+                    case AnalysisLinreg analysisLinreg:
+                        analysisLinreg.Result = _rservice.CalculateLinreg(analysisLinreg) ?? new();
+                        break;
+                    case AnalysisLogistReg analysisLogistReg:
+                        analysisLogistReg.Result = _rservice.CalculateLogistReg(analysisLogistReg) ?? new();
+                        break;
+                    default:
+                        break;
+                }
+
+                if (analysis.Value.Result.Count == 0)
+                {
+                    WeakReferenceMessenger.Default.Send(new BatchAnalyzeMessage()
+                    {
+                        Id = analysis.Key,
+                        Success = false,
+                        Message = "Could not calculate a result!"
+                    });
+                    continue;
+                }
+                    
+                foreach (var groupByVariable in analysis.Value.GroupBy)
+                {
+                    var valueLabels = _rservice.GetValueLabels(groupByVariable.Name);
+                    if (valueLabels != null)
+                    {
+                        analysis.Value.ValueLabels.Add(groupByVariable.Name, valueLabels);
+                    }
+                }
+
+                analysis.Value.ResultAt = DateTime.Now;
+                analysis.Value.ResultDuration = (analysis.Value.ResultAt! - beforeCalculation).Value.TotalSeconds;
+                    
+                WeakReferenceMessenger.Default.Send(new BatchAnalyzeMessage() { Id = analysis.Key, Success = true, Message = "Success!" });
+            }
+
+            e.Result = analyses;
+        }
+    }
+
+    public class BatchAnalyzeMessage
+    {
+        public int Id { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = String.Empty;
+    }
+}
---
+++ TestLSAnalyzer/Services/TestBatchAnalyze.cs
@@ -0,0 +1,289 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using LSAnalyzer.Models;
+using LSAnalyzer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit.Sdk;
+
+namespace TestLSAnalyzer.Services
+{
+    [Collection("Sequential")]
+    public class TestBatchAnalyze
+    {
+        [Fact]
+        public async Task TestRunBatchSendsMessage()
+        {
+            Rservice rservice = new(new());
+            Assert.True(rservice.Connect(), "R must also be available for tests");
+
+            AnalysisConfiguration analysisConfiguration = new()
+            {
+                DatasetType = new() { },
+                FileName = "dummy.sav",
+            };
+
+            BatchAnalyze batchAnalyze = new(rservice);
+
+            int messageCounter = 0;
+            BatchAnalyzeMessage? lastMessage = null;
+            WeakReferenceMessenger.Default.Register<BatchAnalyzeMessage>(this, (r, m) =>
+            {
+                messageCounter++;
+                lastMessage = m;
+            });
+
+            batchAnalyze.RunBatch(new()
+            {
+                { 1, new AnalysisUnivar(analysisConfiguration) }, 
+                { 2, new AnalysisFreq(analysisConfiguration) }, 
+            }, true);
+            await Task.Delay(100);
+
+            Assert.Equal(2, messageCounter);
+            Assert.NotNull(lastMessage);
+            Assert.False(lastMessage.Success);
+        }
+
+        [Fact]
+        public async Task TestRunBatchWithReloadingFiles()
+        {
+            Rservice rservice = new(new());
+            Assert.True(rservice.Connect(), "R must also be available for tests");
+
+            AnalysisConfiguration analysisConfigurationInvalid = new()
+            {
+                DatasetType = new() { },
+                FileName = "dummy.sav",
+            };
+            AnalysisConfiguration analysisConfigurationNmi10Rep5 = new()
+            {
+                FileName = Path.Combine(AssemblyDirectory, "_testData", "test_nmi10_nrep5.sav"),
+                DatasetType = new()
+                {
+                    Weight = "wgt",
+                    NMI = 10, MIvar = "mi",
+                    Nrep = 5, RepWgts = "repwgt", FayFac = 0.5,
+                },
+                ModeKeep = true,
+            };
+            AnalysisConfiguration analysisConfigurationNmi10Multicat = new()
+            {
+                FileName = Path.Combine(AssemblyDirectory, "_testData", "test_nmi10_multicat.sav"),
+                DatasetType = new()
+                {
+                    Weight = "wgt",
+                    NMI = 10, MIvar = "mi",
+                    Nrep = 1,
+                },
+                ModeKeep = false,
+            };
+
+            BatchAnalyze batchAnalyze = new(rservice);
+
+            List<BatchAnalyzeMessage> messages = new();
+            WeakReferenceMessenger.Default.Register<BatchAnalyzeMessage>(this, (r, m) =>
+            {
+                messages.Add(m);
+            });
+
+            Dictionary<int, Analysis> analyses = new()
+            {
+                { 1, new AnalysisUnivar(analysisConfigurationNmi10Rep5) {
+                        Vars = new() { new(1, "doesntExist", false) },
+                    } },
+                { 2, new AnalysisUnivar(analysisConfigurationNmi10Rep5) {
+                        Vars = new() { new(1, "x", false) },
+                        GroupBy = new() { new(2, "cat", false) },
+                    }},
+                { 3, new AnalysisFreq(analysisConfigurationNmi10Multicat) {
+                        Vars = new() { new(1, "doesntExist", false) },
+                    } },
+                { 4, new AnalysisFreq(analysisConfigurationNmi10Multicat) {
+                        Vars = new() { new(1, "item1", false), new(2, "item2", false) },
+                        GroupBy = new() { new(2, "cat", false) },
+                        CalculateBivariate = true,
+                    } },
+                { 5, new AnalysisCorr(analysisConfigurationInvalid) {
+                        Vars = new() { new(1, "dummy", false) },
+                    } },
+            };
+
+            batchAnalyze.RunBatch(analyses, false);
+
+            while (messages.Count < 5)
+            {
+                await Task.Delay(100);
+            }
+
+            Assert.False(messages[0].Success);
+            Assert.True(messages[1].Success);
+            Assert.False(messages[2].Success);
+            Assert.True(messages[3].Success);
+            Assert.False(messages[4].Success);
+        }
+
+        [Fact]
+        public async Task TestRunBatchOnCurrentFileModeKeep()
+        {
+            Rservice rservice = new(new());
+            Assert.True(rservice.Connect(), "R must also be available for tests");
+
+            AnalysisConfiguration analysisConfigurationNmi10Rep5 = new()
+            {
+                FileName = Path.Combine(AssemblyDirectory, "_testData", "test_nmi10_nrep5.sav"),
+                DatasetType = new()
+                {
+                    Weight = "wgt",
+                    NMI = 10,
+                    MIvar = "mi",
+                    Nrep = 5,
+                    RepWgts = "repwgt",
+                    FayFac = 0.5,
+                },
+                ModeKeep = true,
+            };
+            AnalysisConfiguration analysisConfigurationNmi10Multicat = new()
+            {
+                FileName = Path.Combine(AssemblyDirectory, "_testData", "test_nmi10_multicat.sav"),
+                DatasetType = new()
+                {
+                    Weight = "wgt",
+                    NMI = 10,
+                    MIvar = "mi",
+                    Nrep = 1,
+                },
+                ModeKeep = false,
+            };
+
+            Assert.True(rservice.TestAnalysisConfiguration(analysisConfigurationNmi10Rep5));
+
+            BatchAnalyze batchAnalyze = new(rservice);
+
+            List<BatchAnalyzeMessage> messages = new();
+            WeakReferenceMessenger.Default.Register<BatchAnalyzeMessage>(this, (r, m) =>
+            {
+                messages.Add(m);
+            });
+
+            Dictionary<int, Analysis> analyses = new()
+            {
+                { 1, new AnalysisUnivar(analysisConfigurationNmi10Rep5) {
+                        Vars = new() { new(1, "doesntExist", false) },
+                    } },
+                { 2, new AnalysisUnivar(analysisConfigurationNmi10Rep5) {
+                        Vars = new() { new(1, "x", false) },
+                        GroupBy = new() { new(2, "cat", false) },
+                    }},
+                { 3, new AnalysisFreq(analysisConfigurationNmi10Multicat) {
+                        Vars = new() { new(1, "cat", false) },
+                    } },
+                { 4, new AnalysisFreq(analysisConfigurationNmi10Multicat) {
+                        Vars = new() { new(1, "item1", false), new(2, "item2", false) },
+                        GroupBy = new() { new(2, "cat", false) },
+                        CalculateBivariate = true,
+                    } },
+            };
+
+            batchAnalyze.RunBatch(analyses, true, true);
+
+            while (messages.Count < 4)
+            {
+                await Task.Delay(100);
+            }
+
+            Assert.False(messages[0].Success);
+            Assert.True(messages[1].Success);
+            Assert.True(messages[2].Success);
+            Assert.False(messages[3].Success);
+        }
+
+        [Fact]
+        public async Task TestRunBatchOnCurrentFileModeBuild()
+        {
+            Rservice rservice = new(new());
+            Assert.True(rservice.Connect(), "R must also be available for tests");
+
+            AnalysisConfiguration analysisConfigurationNmi10Rep5 = new()
+            {
+                FileName = Path.Combine(AssemblyDirectory, "_testData", "test_nmi10_nrep5.sav"),
+                DatasetType = new()
+                {
+                    Weight = "wgt",
+                    NMI = 10,
+                    MIvar = "mi",
+                    Nrep = 5,
+                    RepWgts = "repwgt",
+                    FayFac = 0.5,
+                },
+                ModeKeep = true,
+            };
+            AnalysisConfiguration analysisConfigurationNmi10Multicat = new()
+            {
+                FileName = Path.Combine(AssemblyDirectory, "_testData", "test_nmi10_multicat.sav"),
+                DatasetType = new()
+                {
+                    Weight = "wgt",
+                    NMI = 10,
+                    MIvar = "mi",
+                    Nrep = 1,
+                },
+                ModeKeep = false,
+            };
+
+            Assert.True(rservice.TestAnalysisConfiguration(analysisConfigurationNmi10Multicat));
+
+            BatchAnalyze batchAnalyze = new(rservice);
+
+            List<BatchAnalyzeMessage> messages = new();
+            WeakReferenceMessenger.Default.Register<BatchAnalyzeMessage>(this, (r, m) =>
+            {
+                messages.Add(m);
+            });
+
+            Dictionary<int, Analysis> analyses = new()
+            {
+                { 1, new AnalysisUnivar(analysisConfigurationNmi10Rep5) {
+                        Vars = new() { new(1, "item1", false) },
+                    } },
+                { 2, new AnalysisUnivar(analysisConfigurationNmi10Rep5) {
+                        Vars = new() { new(1, "x", false) },
+                        GroupBy = new() { new(2, "cat", false) },
+                    }},
+                { 3, new AnalysisFreq(analysisConfigurationNmi10Multicat) {
+                        Vars = new() { new(1, "cat", false) },
+                    } },
+                { 4, new AnalysisFreq(analysisConfigurationNmi10Multicat) {
+                        Vars = new() { new(1, "item1", false), new(2, "item2", false) },
+                        GroupBy = new() { new(2, "cat", false) },
+                        CalculateBivariate = true,
+                    } },
+            };
+
+            batchAnalyze.RunBatch(analyses, true, false);
+
+            while (messages.Count < 4)
+            {
+                await Task.Delay(100);
+            }
+
+            Assert.False(messages[0].Success);
+            Assert.False(messages[1].Success);
+            Assert.True(messages[2].Success);
+            Assert.True(messages[3].Success);
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().Location;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path)!;
+            }
+        }
+    }
+}
