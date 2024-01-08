0f05b6d7af6d7423416c5dd34c7f1accb7910620
Konrad Oberwimmer <k.oberwimmer@bifie.at>
2024-01-08 11:34:57 +0100

    en route to UI test

--- TestLSAnalyzer/TestApp.cs
+++ TestLSAnalyzer/TestApp.cs
@@ -1,29 +1,64 @@
 ﻿using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Definitions;
+using FlaUI.Core.Tools;
 using FlaUI.UIA3;
-using System;
 using System.Reflection;
 
 namespace TestLSAnalyzer
 {
     [Collection("Sequential")]
-    public sealed class TestApp : IDisposable
+    public class TestApp
     {
-        private readonly Application _app = Application.Launch(@"LSAnalyzer.exe");
-
         [Fact]
         public void TestWorkflowPirlsAustria()
         {
-            using var automation = new UIA3Automation();
-            Window window = _app.GetMainWindow(automation, TimeSpan.FromSeconds(3));
+            var app = Application.Launch(Path.Combine(AssemblyDirectory, "LSAnalyzer.exe"));
 
-            Assert.True(window != null, "App did not start - bear in mind that R and BIFIEsurvey are necessary for app tests too");
-        }
+            using UIA3Automation automation = new();
+            ConditionFactory cf = new(new UIA3PropertyLibrary());
 
-        public void Dispose()
-        {
-            _app.Close();
-            _app.Dispose();
+            // start LSAnalyzer
+            var mainWindow = app.GetMainWindow(automation, TimeSpan.FromSeconds(5));
+            Assert.True(mainWindow != null, "App did not start - bear in mind that R and BIFIEsurvey are necessary for app tests too");
+
+            // open up Select File dialog
+            var fileMenu = mainWindow.FindFirstDescendant(cf.ByName("File")).AsMenuItem();
+            fileMenu.Click();
+            fileMenu.Items.Where(item => item.Name == "Select File ...").First().Click();
+            var selectFileDialog = Retry.WhileNull(() => app.GetAllTopLevelWindows(automation).Where(window => window.Title == "Select file for analyses").FirstOrDefault(), TimeSpan.FromSeconds(2)).Result;
+            Assert.NotNull(selectFileDialog);
+            
+            // get PIRLS Austria data file
+            var openFileDialogButton = selectFileDialog.FindFirstDescendant(cf.ByName("Select ...")).AsButton();
+            openFileDialogButton.Click();
+            var openFileDialog = Retry.WhileNull(() => selectFileDialog.ModalWindows.FirstOrDefault(), TimeSpan.FromSeconds(2)).Result;
+            Assert.NotNull(openFileDialog);
+            var filenameTextField = openFileDialog.FindFirstDescendant(cf.ByControlType(ControlType.ComboBox).And(cf.ByName("Dateiname:"))).AsComboBox();
+            Assert.NotNull(filenameTextField);
+            filenameTextField.EditableText = Path.Combine(AssemblyDirectory, "_testData", "test_asgautr4.sav");
+            var openFileButton = openFileDialog.FindFirstDescendant(cf.ByControlType(ControlType.Button).And(cf.ByClassName("Button")).And(cf.ByName("Öffnen"))).AsButton();
+            Assert.NotNull(openFileButton);
+            openFileButton.Click();
+
+            // set PIRLS dataset definition
+            var selectDatasetTypeList = selectFileDialog.FindFirstChild("comboBoxDatasetType").AsComboBox();
+            Assert.NotNull(selectDatasetTypeList);
+            selectDatasetTypeList.Select("PIRLS since 2016 - student level");
+            Assert.True(selectDatasetTypeList.SelectedItem != null, "Dataset type could not be selected - note that 'PIRLS since 2016 - student level' needs to be configured for your user");
+            
+            // load PIRLS Austria data
+            var useFileForAnalysisButton = selectFileDialog.FindFirstDescendant(cf.ByControlType(ControlType.Button).And(cf.ByName("Go!"))).AsButton();
+            Assert.NotNull(useFileForAnalysisButton);
+            useFileForAnalysisButton.WaitUntilEnabled();
+            useFileForAnalysisButton.Focus();
+            useFileForAnalysisButton.Click();
+            Retry.WhileNotNull(() => app.GetAllTopLevelWindows(automation).Where(window => window.Title == "Select file for analyses").FirstOrDefault(), TimeSpan.FromSeconds(10));
+            
+            // close app
+            mainWindow.Close();
+            app.Dispose();
         }
 
         public static string AssemblyDirectory
