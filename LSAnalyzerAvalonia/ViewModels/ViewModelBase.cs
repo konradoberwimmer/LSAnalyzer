127836be6536f81b3417f58ccba44ec18e2e7f56
konrad <konrad.oberwimmer@gmail.com>
2025-03-09 19:38:08 +0100

    set up the Avalonia project

---
+++ LSAnalyzerAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LSAnalyzerAvalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ TestLSAnalyzerAvalonia/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace TestLSAnalyzerAvalonia;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
