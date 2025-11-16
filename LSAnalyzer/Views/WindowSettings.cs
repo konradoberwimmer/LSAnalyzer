f770cfd8a4c1ea32d262c54d52d3314b10d5fc4b
konradoberwimmer <konrad.oberwimmer@gmail.com>
2025-11-16 18:16:46 +0100

    Use WindowSettings for RequestAnalysisFreq.xaml and RequestAnalysisPercentiles.xaml

--- LSAnalyzer/Views/WindowSettings.cs
+++ LSAnalyzer/Views/WindowSettings.cs
@@ -6,7 +6,9 @@ public class WindowSettings
 {
     private static Dictionary<string, (int width, int height)> _settings = new()
     {
-        { "Univar", (width: 800, height: 600) }
+        { "Univar", (width: 800, height: 600) },
+        { "Freq", (width: 800, height: 600) },
+        { "Percentiles", (width: 800, height: 600) },
     };
 
     public static int WidthUnivar
@@ -28,6 +30,50 @@ public class WindowSettings
             var currentSettings = _settings["Univar"];
             currentSettings.height = value;
             _settings["Univar"] = currentSettings;
+        }
+    }
+
+    public static int WidthFreq
+    {
+        get => _settings["Freq"].width;
+        set
+        {
+            var currentSettings = _settings["Freq"];
+            currentSettings.width = value;
+            _settings["Freq"] = currentSettings;
+        }
+    }
+
+    public static int HeightFreq
+    {
+        get => _settings["Freq"].height;
+        set
+        {
+            var currentSettings = _settings["Freq"];
+            currentSettings.height = value;
+            _settings["Freq"] = currentSettings;
+        }
+    }
+
+    public static int WidthPercentiles
+    {
+        get => _settings["Percentiles"].width;
+        set
+        {
+            var currentSettings = _settings["Percentiles"];
+            currentSettings.width = value;
+            _settings["Percentiles"] = currentSettings;
+        }
+    }
+
+    public static int HeightPercentiles
+    {
+        get => _settings["Percentiles"].height;
+        set
+        {
+            var currentSettings = _settings["Percentiles"];
+            currentSettings.height = value;
+            _settings["Percentiles"] = currentSettings;
         }
     }
 }
